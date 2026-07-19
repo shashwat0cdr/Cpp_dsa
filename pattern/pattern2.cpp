6ccf516f13a647ab1ec4f94f4fac08a5efe74809
Shashwat Gujrati <shashwatgujrati@gmail.com>
2026-07-19 16:59:58 +0530

    first commit

---
+++ pattern/pattern2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // int n;
+    // cout << "enter a n: ";
+    // cin >> n;
+    // int row = 1;
+    // int count = 1;
+    // while (row <= n)
+    // {
+    //     int col = 1;
+    //     while (col <= row)
+    //     {
+    //         cout << count<< " ";
+    //         count++;
+    //         col++;
+    //     }
+    //     cout << endl;
+    //     row++;
+    // }
+
+    // //star pattern
+    // int n;
+    // cout << "n: ";
+    // cin >> n;
+    // int i = 1;
+    // while (i<=n){
+    //     int j = 1;
+    //     while(j<=i){
+    //         cout << "* ";
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+    
+    return 0;
+}
