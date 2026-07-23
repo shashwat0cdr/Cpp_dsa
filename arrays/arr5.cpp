0e013d83d505835587dfb52d6f03a4781cb8cd49
Shashwat Gujrati <shashwatgujrati@gmail.com>
2026-07-23 17:40:00 +0530

    array

---
+++ arrays/arr5.cpp
@@ -0,0 +1,14 @@
+// finding an unique element in array
+#include <iostream>
+using namespace std;
+
+int unique(int *arr, int size)
+{
+    int ans = 0;
+    for (int i = 0; i < size; i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    return ans;
+}
+
