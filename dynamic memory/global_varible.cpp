324fe7f5f631d68fd324037869e412886341afc4
Shashwat Gujrati <shashwatgujrati@gmail.com>
2026-08-25 17:35:27 +0530

    dsa

--- dynamic memory/global_varible.cpp
+++ dynamic memory/global_varible.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-// global variable 
+// global variable
 int score = 15;
 
-void a(int& i)
+void a(int &i)
 {
-    
+
     cout << score << endl;
     cout << i << endl;
-    //b(i);
+    // b(i);
 }
 
-void b(int& i)
+void b(int &i)
 {
     cout << i << endl;
 }
@@ -21,13 +21,11 @@ int main()
 {
     // local variable
     int i = 5;
-   // a(i);
+    a(i);
     {
         int i = 2;
         cout << i << endl;
-
     }
-
 
     return 0;
 }
